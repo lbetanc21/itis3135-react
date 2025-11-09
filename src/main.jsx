b55353d6083c053628c4fd4141f248770ec02edd
lbetanc21 <lbetanc2@charlotte.edu>
2025-11-09 14:17:42 -0500

    .

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { HashRouter } from 'react-router-dom'
 import App from './App.jsx'
+import './styles/main.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </StrictMode>
 )

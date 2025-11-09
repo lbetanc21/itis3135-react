92e4b5e2e1a69a6ba248fea76255f2df3dd68643
lbetanc21 <lbetanc2@charlotte.edu>
2025-11-09 14:19:23 -0500

    Update Nav.jsx

---
+++ src/components/Nav.jsx
@@ -0,0 +1,13 @@
+import { NavLink } from 'react-router-dom'
+
+export default function Nav() {
+  return (
+    <nav className="primary-nav" aria-label="Primary">
+      <ul className="nav-list container">
+        <li><NavLink to="/" end>Home</NavLink></li>
+        <li><NavLink to="/introduction">Introduction</NavLink></li>
+        <li><NavLink to="/contract">Contract</NavLink></li>
+      </ul>
+    </nav>
+  )
+}
